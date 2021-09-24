a736c8fbf672daf57612fbbb89bc7c1ac276a806
ashennova <kennerve14@gmail.com>
2021-09-24 14:33:01 +0800

    added devise and styling

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,9 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+// const webpack = require("webpack")
+
+// environment.plugins.append("Provide, new webpackProvidePlugin({
+//     Popper: ['popper.js', 'default']
+// }))
